a0b1dd5d927f660c4cda6dcfcd2e619552281545
Ryan Marinoff <ryan.marinoff@outlook.com>
2020-02-18 17:42:29 -0500

    Start of the project

---
+++ back/ers-back/src/main/java/com/revature/rmarinoff/ersback/ExpenseReimbursementSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.revature.rmarinoff.ersback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReimbursementSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReimbursementSystemBackApplication.class, args);
+	}
+
+}
---
+++ back/ers-back/src/test/java/com/revature/rmarinoff/ersback/ExpenseReimbursementSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.rmarinoff.ersback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReimbursementSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
