b60a4b9723f874b9f67ac4a6b71f0d8fc53ebc65
Ryan Marinoff <ryan.marinoff@outlook.com>
2020-02-21 09:18:28 -0500

    working towards login component completion

--- frontend/ERS/src/app/app.module.ts
+++ frontend/ERS/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './login/login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
